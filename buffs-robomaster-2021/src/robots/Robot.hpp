2ec4e5dfeda117350c4924c6813452ed41dd6ffe
BenPeraCU <73373478+BenPeraCU@users.noreply.github.com>
2021-02-19 17:45:51 -0700

    testing bens commit rights

--- buffs-robomaster-2021/src/robots/Robot.hpp
+++ buffs-robomaster-2021/src/robots/Robot.hpp
@@ -1,6 +1,7 @@
 #include <subsystems/Subsystem.hpp>
 
 class Robot : buffs-robomaster-2021::lib::robots {
+    //class description
     public:
         Robot(int id);
         void addSubsystem(Subsystem subsystem);
