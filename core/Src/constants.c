44e84de66b670af6b06701f0b54b6611786e15da
Isaac Wilcove <isaac@wilcove.com>
2021-05-10 12:46:23 -0700

    enable turret

--- core/Src/constants.c
+++ core/Src/constants.c
@@ -6,8 +6,8 @@ const float M_PI = 3.1415926;
 // Controller
 const float M_CONTROLLER_JOYSTICK_SCALE = 660.0;
 const int M_CONTROLLER_SWITCH_STATES[] = {0, 1, 2};
-const int M_CONTROLLER_X_AXIS = 2;
-const int M_CONTROLLER_Y_AXIS = 3;
+const int M_CONTROLLER_X_AXIS = 3;
+const int M_CONTROLLER_Y_AXIS = 2;
 const int M_CONTROLLER_ROTATION_AXIS = 1;
 
 // M3508 Motor
