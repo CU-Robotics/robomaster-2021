cba834d29f7f7da1b735338522be08ae63cbeff6
Isaac Wilcove <isaac@wilcove.com>
2021-05-10 13:14:50 -0700

    bruh2

--- core/Src/constants.c
+++ core/Src/constants.c
@@ -1,19 +1,19 @@
 #include "constants.h"
 
 // Mathematical Constants
-const float M_PI = 3.1415926;
+const float M_PI = 3.1415926f;
 
 // Controller
-const float M_CONTROLLER_JOYSTICK_SCALE = 660.0;
+const float M_CONTROLLER_JOYSTICK_SCALE = 660.0f;
 const int M_CONTROLLER_SWITCH_STATES[] = {0, 1, 2};
 const int M_CONTROLLER_X_AXIS = 3;
 const int M_CONTROLLER_Y_AXIS = 2;
 const int M_CONTROLLER_ROTATION_AXIS = 1;
 
 // M3508 Motor
-const float M_MOTOR_M3508_VOLTAGE_SCALE = 16384.0; 
-const float M_ENCODER_M3508_SCALE = 8191.0;
+const float M_MOTOR_M3508_VOLTAGE_SCALE = 16384.0f; 
+const float M_ENCODER_M3508_SCALE = 8191.0f;
 
 // GM6020 Motor
-const float M_MOTOR_GM6020_VOLTAGE_SCALE = 30000.0;
-const float M_ENCODER_GM6020_SCALE = 8191.0;
+const float M_MOTOR_GM6020_VOLTAGE_SCALE = 30000.0f;
+const float M_ENCODER_GM6020_SCALE = 8191.0f;
