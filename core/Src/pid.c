e9c6a20b70adee251638e3749c2e75db3e265829
Isaac Wilcove <isaac@wilcove.com>
2021-05-10 13:53:30 -0700

    fix errors

--- core/Src/pid.c
+++ core/Src/pid.c
@@ -7,15 +7,9 @@ float calculateProportional(float currentPosition, float setpoint, PIDProfile pr
     float error = setpoint - currentPosition;
     float error2 = M_ENCODER_GM6020_SCALE + error;
 	
-<<<<<<< HEAD
-    if (abs(error2) < (error)){
-        error = error2;
-    }
-=======
 		if(abs(error2) < abs(error)){
 			error = error2;
 		}
->>>>>>> bc3490223c17b24004b727bec086b483b1fa7b87
     // Calculate correction and return
     return error * profile.kP + profile.kF;
 }
