99605cdc1e767170949d89decab19e4322dc0870
Isaac Wilcove <isaac@wilcove.com>
2021-06-06 00:45:03 -0700

    Added quickturn to turret

--- core/Src/turret.c
+++ core/Src/turret.c
@@ -20,6 +20,9 @@ bool zeroed;
 int flywheelSpinupTracker;
 int unjamTracker;
 int unjamDirection;
+bool leftQuickturnAlreadyPressed;
+bool rightQuickturnAlreadyPressed;
+
 int pitchRotations;
 int prevPitchPosition;
 int pitchHistory[M_ZERO_HARDSTOP_TIME_THRESHOLD];
@@ -30,111 +33,129 @@ float pitchSetpoint = 0;
 float tempPitchPosition = 0;
 
 void turretInit() {
-    // PID Profiles containing tuning parameters.
-    yawProfile.kP = 16.0f / (8 * M_PI);
-    yawProfile.kI = 0.00025f / (8 * M_PI);
-    yawProfile.kD = 2.0f / (8 * M_PI);
+  // PID Profiles containing tuning parameters.
+  yawProfile.kP = 16.0f / (8 * M_PI);
+  yawProfile.kI = 0.00025f / (8 * M_PI);
+  yawProfile.kD = 2.0f / (8 * M_PI);
 
-    pitchProfile.kP = 10.0f / (8 * M_PI);
-    pitchProfile.kI = 0.0f / (8 * M_PI);
-    pitchProfile.kD = 0.0f / (8 * M_PI);
-		pitchProfile.kF = 4.20f / (8 * M_PI);
-	
-    // PID States
-    yawState.lastError = 0;
-    pitchState.lastError = 0;
+  pitchProfile.kP = 10.0f / (8 * M_PI);
+  pitchProfile.kI = 0.0f / (8 * M_PI);
+  pitchProfile.kD = 0.0f / (8 * M_PI);
+  pitchProfile.kF = 4.20f / (8 * M_PI);
 
-    zeroed = false;
-    flywheelSpinupTracker = 0;
-    unjamTracker = 0;
-    unjamDirection = 1;
+  // PID States
+  yawState.lastError = 0;
+  pitchState.lastError = 0;
 
-    pitchRotations = 0;
-    prevPitchPosition = 0;
-    //Fill pitch history with values preventing false positive hardstop reading
-    for(int i = 0; i < M_ZERO_HARDSTOP_TIME_THRESHOLD; i++)
-        pitchHistory[i] = 30 * i;
+  zeroed = false;
+  flywheelSpinupTracker = 0;
+  unjamTracker = 0;
+  unjamDirection = 1;
+  leftQuickturnAlreadyPressed = false;
+  rightQuickturnAlreadyPressed = false;
+
+  pitchRotations = 0;
+  prevPitchPosition = 0;
+  //Fill pitch history with values preventing false positive hardstop reading
+  for(int i = 0; i < M_ZERO_HARDSTOP_TIME_THRESHOLD; i++)
+    pitchHistory[i] = 30 * i;
 }
 
 void turretLoop(const RC_ctrl_t* control_input, int deltaTime) {
-    if (zeroed) {
-        // Float yawSetpoint = M_PI * (control_input->rc.ch[M_CONTROLLER_X_AXIS] / (M_CONTROLLER_JOYSTICK_SCALE));
-        // Float pitchSetpoint = M_PI * (control_input->rc.ch[M_CONTROLLER_Y_AXIS] / M_CONTROLLER_JOYSTICK_SCALE);
-				
-				// Experimental mouse gimbal control
-				yawSetpoint += deltaTime * M_PI * (control_input->mouse.x / (M_MOUSE_X_SCALE));
-        pitchSetpoint += deltaTime * M_PI * (control_input->mouse.y / (M_MOUSE_Y_SCALE));
+  if (zeroed) {
+    /* Turret Code */
+    // Experimental mouse gimbal control
+    yawSetpoint += deltaTime * M_PI * (control_input->mouse.x / (M_MOUSE_X_SCALE));
+    pitchSetpoint += deltaTime * M_PI * (control_input->mouse.y / (M_MOUSE_Y_SCALE));
 
-        Turret turret = calculateTurret(yawSetpoint, pitchSetpoint, yawProfile, pitchProfile, &yawState, &pitchState);
-
-        float feederSpeed = 0.0f;
-
-        // Fire
-        if (control_input->mouse.press_l) {
-          fric_on((uint16_t) ((M_SNAIL_SPEED_OFFSET + M_SNAIL_SPEED_SCALE) * M_SHOOTER_CURRENT_PERCENT));
-          flywheelSpinupTracker += deltaTime;
-          if (flywheelSpinupTracker >= M_SHOOTER_DELAY) {
-            feederSpeed = M_M2006_CURRENT_SCALE * -M_FEEDER_CURRENT_PERCENT;
-            flywheelSpinupTracker = M_SHOOTER_DELAY;
-          }
-        // Prespin
-        } else if(control_input->mouse.press_r){
-					fric_on((uint16_t) ((M_SNAIL_SPEED_OFFSET + M_SNAIL_SPEED_SCALE) * M_SHOOTER_CURRENT_PERCENT));
-        // Unjam
-				} else if (control_input->key.v['r']) {
-          if (unjamTracker >= M_SHOOTER_UNJAM_PERIOD || unjamTracker <= -M_SHOOTER_UNJAM_PERIOD)
-            unjamDirection = -unjamDirection;
-          fric_on((uint16_t) ((M_SNAIL_SPEED_OFFSET + M_SNAIL_SPEED_SCALE * unjamDirection) * M_SHOOTER_CURRENT_PERCENT));
-          feederSpeed = M_M2006_CURRENT_SCALE * M_FEEDER_CURRENT_PERCENT * unjamDirection;
-          unjamTracker += unjamDirection * deltaTime;
-        // Base State
-        } else {
-          fric_on((uint16_t) (M_SNAIL_SPEED_OFFSET));
-          flywheelSpinupTracker -= deltaTime;
-          if (flywheelSpinupTracker < 0) { flywheelSpinupTracker = 0 };
-          unjamTracker = 0;
-          unjamDirection = 1;
-        }
-
-        CAN_cmd_gimbal_working(turret.yaw * M_GM6020_VOLTAGE_SCALE, turret.pitch * M_M3508_CURRENT_SCALE, feederSpeed, 0);
-    /* Zero Sequence */
+    // Left Quickturn
+    if (control_input->key.v['1'] && !leftQuickturnAlreadyPressed) {
+      yawSetpoint -= (M_PI / 2.0f);
+      leftQuickturnAlreadyPressed = true;
     } else {
-				// Move pitch motor towards front endstop
-        CAN_cmd_gimbal_working(0, 4000, 0, 0);
-				
-				// Capture encoder value
-				float pitchPosition = (get_pitch_gimbal_motor_measure_point()->ecd);
-
-				// Rotate pitch history
-				for (int i = 0; i < M_ZERO_HARDSTOP_TIME_THRESHOLD - 1; i++){
-					pitchHistory[i] = pitchHistory[i+1];
-				}
-				pitchHistory[M_ZERO_HARDSTOP_TIME_THRESHOLD - 1] = pitchPosition;
-
-				// Check if at hardstop
-				if(isM3508AtHardstop(pitchHistory)){
-						zeroed = true;
-						pitchOffset = pitchPosition;
-				}
+      leftQuickturnAlreadyPressed = false;
     }
+    // Right Quickturn
+    if (control_input->key.v['2'] && !rightQuickturnAlreadyPressed) {
+      yawSetpoint += (M_PI / 2.0f);
+      rightQuickturnAlreadyPressed = true;
+    } else {
+      rightQuickturnAlreadyPressed = false;
+    }
+
+    Turret turret = calculateTurret(yawSetpoint, pitchSetpoint, yawProfile, pitchProfile, &yawState, &pitchState);
+
+    /* Shooter Code */
+    float feederSpeed = 0.0f;
+
+    // Fire
+    if (control_input->mouse.press_l) {
+      fric_on((uint16_t) ((M_SNAIL_SPEED_OFFSET + M_SNAIL_SPEED_SCALE) * M_SHOOTER_CURRENT_PERCENT));
+      flywheelSpinupTracker += deltaTime;
+      if (flywheelSpinupTracker >= M_SHOOTER_DELAY) {
+        feederSpeed = M_M2006_CURRENT_SCALE * -M_FEEDER_CURRENT_PERCENT;
+        flywheelSpinupTracker = M_SHOOTER_DELAY;
+      }
+    // Prespin
+    } else if(control_input->mouse.press_r){
+      fric_on((uint16_t) ((M_SNAIL_SPEED_OFFSET + M_SNAIL_SPEED_SCALE) * M_SHOOTER_CURRENT_PERCENT));
+    // Unjam
+    } else if (control_input->key.v['r']) {
+      if (unjamTracker >= M_SHOOTER_UNJAM_PERIOD || unjamTracker <= -M_SHOOTER_UNJAM_PERIOD)
+        unjamDirection = -unjamDirection;
+      fric_on((uint16_t) ((M_SNAIL_SPEED_OFFSET + M_SNAIL_SPEED_SCALE * unjamDirection) * M_SHOOTER_CURRENT_PERCENT));
+      feederSpeed = M_M2006_CURRENT_SCALE * M_FEEDER_CURRENT_PERCENT * unjamDirection;
+      unjamTracker += unjamDirection * deltaTime;
+    // Base State
+    } else {
+      fric_on((uint16_t) (M_SNAIL_SPEED_OFFSET));
+      flywheelSpinupTracker -= deltaTime;
+      if (flywheelSpinupTracker < 0) { flywheelSpinupTracker = 0 };
+      unjamTracker = 0;
+      unjamDirection = 1;
+    }
+
+    /* Set motor output values */
+    CAN_cmd_gimbal_working(turret.yaw * M_GM6020_VOLTAGE_SCALE, turret.pitch * M_M3508_CURRENT_SCALE, feederSpeed, 0);
+
+  /* Zero Sequence */
+  } else {
+    // Move pitch motor towards front endstop
+    CAN_cmd_gimbal_working(0, 4000, 0, 0);
+    
+    // Capture encoder value
+    float pitchPosition = (get_pitch_gimbal_motor_measure_point()->ecd);
+
+    // Rotate pitch history
+    for (int i = 0; i < M_ZERO_HARDSTOP_TIME_THRESHOLD - 1; i++){
+      pitchHistory[i] = pitchHistory[i+1];
+    }
+    pitchHistory[M_ZERO_HARDSTOP_TIME_THRESHOLD - 1] = pitchPosition;
+
+    // Check if at hardstop
+    if(isM3508AtHardstop(pitchHistory)){
+      zeroed = true;
+      pitchOffset = pitchPosition;
+    }
+  }
 }
 
 Turret calculateTurret(float yawAngle, float pitchAngle, PIDProfile yawPIDProfile, PIDProfile pitchPIDProfile, PIDState *yawPIDState, PIDState *pitchPIDState) {
-    // Captures positions in encoders native format
-		float yawPosition = (get_yaw_gimbal_motor_measure_point()->ecd);
-    float pitchPosition = (get_pitch_gimbal_motor_measure_point()->ecd);
+  // Captures positions in encoders native format
+  float yawPosition = (get_yaw_gimbal_motor_measure_point()->ecd);
+  float pitchPosition = (get_pitch_gimbal_motor_measure_point()->ecd);
 
-		// Keeps track of true position including reduction
-    pitchRotations += countRotationsM3508(pitchPosition, prevPitchPosition);
-    prevPitchPosition = pitchPosition;
-    pitchPosition = (pitchPosition + pitchRotations * M_M3508_ENCODER_SCALE - pitchOffset) * M_M3508_REDUCTION_RATIO;
-		tempPitchPosition = (2.0f * M_PI * pitchPosition) / M_M3508_ENCODER_SCALE;
-	
-    Turret turret;
+  // Keeps track of true position including reduction
+  pitchRotations += countRotationsM3508(pitchPosition, prevPitchPosition);
+  prevPitchPosition = pitchPosition;
+  pitchPosition = (pitchPosition + pitchRotations * M_M3508_ENCODER_SCALE - pitchOffset) * M_M3508_REDUCTION_RATIO;
+  tempPitchPosition = (2.0f * M_PI * pitchPosition) / M_M3508_ENCODER_SCALE;
 
-		// Calculate turret thrusts using PID with input of radians
-    turret.yaw = calculatePID((2.0f * M_PI * yawPosition) / M_GM6020_ENCODER_SCALE, yawAngle, yawPIDProfile, yawPIDState);
-    turret.pitch = calculatePID_SinFeedforward((2.0f * M_PI * pitchPosition) / M_M3508_ENCODER_SCALE, pitchAngle, pitchPIDProfile, pitchPIDState);
+  Turret turret;
 
-    return turret;
+  // Calculate turret thrusts using PID with input of radians
+  turret.yaw = calculatePID((2.0f * M_PI * yawPosition) / M_GM6020_ENCODER_SCALE, yawAngle, yawPIDProfile, yawPIDState);
+  turret.pitch = calculatePID_SinFeedforward((2.0f * M_PI * pitchPosition) / M_M3508_ENCODER_SCALE, pitchAngle, pitchPIDProfile, pitchPIDState);
+
+  return turret;
 }
