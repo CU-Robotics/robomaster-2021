001aca5117eb89d48adebaf8a3fc701d045624d6
Isaac Wilcove <isaac@wilcove.com>
2021-06-12 14:32:13 -0700

    Reformat time_manip.c

--- core/Src/time_manip.c
+++ core/Src/time_manip.c
@@ -16,15 +16,15 @@
    * This is a ms accurate timer but a different implimentation would be required for more accuracy
  */
 int function_with_interval(void (*function)(int*), int* parameters, int interval) {
-    uint32_t startTick;                     //starting time
-    uint32_t endTick;                       //current time
-    startTick = HAL_GetTick();              //get start time
-    (*function)(parameters);                //run function
-    endTick = HAL_GetTick();                //get end time
-    if (endTick-startTick > M_MASTER_LOOP_TIMEOUT) {      //if the time it took to run the function is greater than desired interval
-        return -1;                            //return error -1
-    } do {                                    
-        endTick = HAL_GetTick();              //update the current time
-    } while(endTick-startTick < interval);  //until desired time interval has been reached
-    return 0;                               //return success
+  uint32_t startTick;                     // starting time
+  uint32_t endTick;                       // current time
+  startTick = HAL_GetTick();              // get start time
+  (*function)(parameters);                // run function
+  endTick = HAL_GetTick();                // get end time
+  if (endTick-startTick > M_MASTER_LOOP_TIMEOUT) { // if the time it took to run the function is greater than desired interval
+    return -1;                          // return error -1
+  } do {                                    
+    endTick = HAL_GetTick();            // update the current time
+  } while(endTick-startTick < interval);  // until desired time interval has been reached
+  return 0;                               // return success
 }
